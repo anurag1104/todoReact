c3e45b48d5fceeed5fcccd79c6806f4e2b367a06
anurag1104 <anurag@sharma.ac>
2025-08-15 17:18:46 +0530

    second commit

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.delete('/api/todos/:id', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0' ,() => console.log(`Server running on port ${PORT}`));
 
 
 //test db connection
